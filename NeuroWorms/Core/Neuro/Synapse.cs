19081cadd5e3b3e76e3f27e5c4a06f18f803a843
Anton Zhuchkov <anton.zh@xor.ai>
2023-10-09 18:26:29 +0300

    update

---
+++ NeuroWorms/Core/Neuro/Synapse.cs
@@ -0,0 +1,19 @@
+﻿namespace NeuroWorms.Core.Neuro
+{
+    internal class Synapse
+    {
+        public double Weight { get; set; }
+        public BasicNeuron From { get; set; }
+
+        public Synapse(double weight, BasicNeuron from)
+        {
+            Weight = weight;
+            From = from;
+        }
+
+        public double GetValue()
+        {
+            return From.Activate() * Weight;
+        }
+    }
+}
